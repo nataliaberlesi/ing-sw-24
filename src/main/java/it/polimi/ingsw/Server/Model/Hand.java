5996b60fcb2c106cacb97f4b13b3fcea75b05cd4
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-03-21 10:25:43 +0100

    Created hand class

---
+++ src/main/java/it/polimi/ingsw/Server/Model/Hand.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.Server.Model;
+
+/**
+ * This is the players hand, it can never have more than three cards,
+ * at the end of each turn the hand must contain three cards.
+ */
+public class Hand {
+    /**
+     * Array of 3 cards
+     */
+    private Card[] cards=new Card[3];
+
+    /**
+     *
+     * @param card that is being drawn and will be added to cards
+     */
+    public void drawCard(Card card){}
+
+    /**
+     *
+     * @param cardIndex is the index of the card that will be removed from card
+     *                  to later be placed on the board
+     * @return card that is in cards[cardIndex]
+     */
+    public Card placeCard(int cardIndex){
+        //Caution, method under construction...
+        return cards[cardIndex];
+    }
+}
