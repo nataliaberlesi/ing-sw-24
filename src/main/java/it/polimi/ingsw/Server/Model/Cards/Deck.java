8e8157c4dba621158e799d397e61722cad08fd7e
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-03-24 18:02:24 +0100

    Made deck iterator

--- src/main/java/it/polimi/ingsw/Server/Model/Cards/Deck.java
+++ src/main/java/it/polimi/ingsw/Server/Model/Cards/Deck.java
@@ -3,28 +3,44 @@ package it.polimi.ingsw.Server.Model.Cards;
 import it.polimi.ingsw.Server.Model.Cards.Card;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Deck of cards, can hold only either gold cards or resource cards
  */
-public class Deck {
+public class Deck implements Iterator<Card> {
     /**
      * cards in deck, can be either only resource cards or only gold cards
      */
     private ArrayList<Card> cards;
-    /**
-     *
-     * @return a card from the deck
-     */
-    public Card draw(){
-        //Caution, method under construction WIP
-        return cards.get(0);
-    }
 
     /**
      * shuffles cards in deck
      */
     public void shuffle(){
 
+    }
+
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration, returns null if cards is empty
+     *
+     */
+    @Override
+    public Card next() {
+        return null;
     }
 }
---
+++ src/test/java/it/polimi/ingsw/Server/Model/Cards/DeckTest.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.Server.Model.Cards;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @org.junit.jupiter.api.Test
+    void draw() {
+    }
+}
