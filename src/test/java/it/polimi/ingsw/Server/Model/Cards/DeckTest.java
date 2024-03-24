7b0665a1cfeeac1d97435b5596df453c1b401ba3
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-03-24 18:59:57 +0100

    expanded deck hasNext() test

--- src/test/java/it/polimi/ingsw/Server/Model/Cards/DeckTest.java
+++ src/test/java/it/polimi/ingsw/Server/Model/Cards/DeckTest.java
@@ -3,16 +3,28 @@ package it.polimi.ingsw.Server.Model.Cards;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckTest {
     /**
-     * testing that if deck has no cards returns false
+     * if deck doesn't have any cards, hasNext() must return false
      */
     @Test
-    void hasNext() {
+    void hasNextEmptyDeckReturnFlase() {
         Deck deck=new Deck(new ArrayList<Card>());
         assertFalse(deck.hasNext());
+    }
+
+    /**
+     * if deck has cards, hasNext() must return true
+     */
+    @Test
+    void hasNextDeckWithCardsReturnTrue(){
+        Card exampleCard=new ResourceCard(null,null,null,null,null);
+        Deck deck=new Deck(new ArrayList<Card>(Collections.singletonList(exampleCard)));
+        assertTrue(deck.hasNext());
     }
 }
