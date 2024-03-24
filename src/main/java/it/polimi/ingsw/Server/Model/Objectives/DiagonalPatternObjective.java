54e8a434031c8645bcb49f2e4ea22baaeb63cc87
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-03-24 13:31:28 +0100

    Collapled DescendingDiagonalObjective.java and IncrementingDiagonalObjective.java into DiagonalPatternObjective

--- src/main/java/it/polimi/ingsw/Server/Model/Objectives/DiagonalPatternObjective.java
+++ src/main/java/it/polimi/ingsw/Server/Model/Objectives/DiagonalPatternObjective.java
@@ -5,30 +5,34 @@ import it.polimi.ingsw.Server.Model.Symbol;
 
 import java.util.ArrayList;
 
-public class IncrementingDiagonalObjective implements Objective {
-
+/**
+ *  objective that assigns 2 points per diagonal pattern of cards of the same color
+ *  on the player's board.
+ *  If the objective looks for green or purple cards, only descending diagonal patterns assign points,
+ *  red and blu cards only assign points for incrementing diagonal pattern.
+ */
+public class DiagonalPatternObjective implements Objective {
     /**
      * number of points assigned for each time descending diagonal pattern of a certain color is found
      *
      */
     private final static int POINTS=2;
-
     /**
      * symbolOfInterest indicates the backSymbol of the cards that won't be filtered out in the updateObjective method
      * the back symbol is an indicator of the card's color
      */
     private final Symbol symbolOfInterest;
-
     /**
      * contains the list of all coordinates present in the board of the cards that contain the symbol of interest
      */
-    private ArrayList<Coordinates> ListOfCoordinatessOfInterest=new ArrayList<>();
+    private ArrayList<Coordinates> ListOfCoordinatesOfInterest=new ArrayList<>();
 
     /**
      *
-     * @param symbolOfInterest indicates the backSymbol of the cards that will be checked
+     * @param symbolOfInterest is the symbol that will be used to filter the cards that will be added
+     *                         to the list of coordinates
      */
-    public IncrementingDiagonalObjective(Symbol symbolOfInterest) {
+    public DiagonalPatternObjective(Symbol symbolOfInterest) {
         this.symbolOfInterest = symbolOfInterest;
     }
 
@@ -45,7 +49,7 @@ public class IncrementingDiagonalObjective implements Objective {
     }
 
     /**
-     * symbolCounter is ignored
+     * Symbol counter is ignored
      * @param symbolCounter map containing number of visible occurrences for each symbol
      * @return number of point earned
      */
