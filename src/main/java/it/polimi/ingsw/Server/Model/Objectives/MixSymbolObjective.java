6e8cb518c1453bba623ba9c0bac5b5ededd3a8ab
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-03-24 13:16:03 +0100

    Added MixSymbolObjective

---
+++ src/main/java/it/polimi/ingsw/Server/Model/Objectives/MixSymbolObjective.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.Server.Model.Objectives;
+
+import it.polimi.ingsw.Server.Model.Coordinates;
+import it.polimi.ingsw.Server.Model.Symbol;
+
+import java.util.ArrayList;
+
+/**
+ * objective that assigns 3 points per set of 3 different Symbols (SCROLL, INK, FEATHER)
+ * visible on the board of the player.
+ */
+public class MixSymbolObjective implements Objective{
+    /**
+     * Points per set of 3 different Symbols (SCROLL, INK, FEATHER)
+     * visible on the board of the player.
+     */
+    private static final int POINTS=3;
+    /**
+     * updates class regarding the color and coordinates of a card that is being placed
+     *
+     * @param cardBackSymbol symbol the card that is being placed
+     * @param coordinates    coordinates of the card that is being placed
+     */
+    @Override
+    public void updateObjective(Symbol cardBackSymbol, Coordinates coordinates) {
+
+    }
+
+    /**
+     * @param symbolCounter map containing number of visible occurrences for each symbol
+     * @return number of point earned
+     */
+    @Override
+    public int calculatePoints(ArrayList<Symbol> symbolCounter) {
+        return 0;
+    }
+}
