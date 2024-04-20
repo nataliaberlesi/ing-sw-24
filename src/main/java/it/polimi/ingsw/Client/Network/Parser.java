00e3ac817e35498fdb563c504fca399a3549dc58
Kevin Vallerin <k.vallerin01@gmail.com>
2024-04-20 14:22:22 +0200

    Parsers: Created Parser singleton objects for Client and Server

---
+++ src/main/java/it/polimi/ingsw/Client/Network/Parser.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.Client.Network;
+
+import com.google.gson.Gson;
+import it.polimi.ingsw.Client.Network.Message;
+
+public class Parser {
+    /**
+     * the gson object used to parse
+     */
+    private static final Gson GSON=new Gson();
+    /**
+     * the singleton instance
+     */
+    private static final Parser INSTANCE=new Parser();
+    private Parser() {
+    }
+    public static Parser getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * parses a string into a message
+     * @param string the string to be parsed
+     * @return the message
+     */
+    public Message toMessage(String string) {
+        return GSON.fromJson(string, Message.class);
+    }
+    /**
+     * parses a message into a string
+     * @param message the message to be parsed
+     * @return the string
+     */
+    public String toString(Message message) {
+        return GSON.toJson(message);
+    }
+
+}
