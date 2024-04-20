486d5854edb9659c4bc7f7da3dd2c6d9b0e297d2
Kevin Vallerin <k.vallerin01@gmail.com>
2024-04-20 14:25:32 +0200

    Message: Created the client-side message class

---
+++ src/main/java/it/polimi/ingsw/Client/Network/Message.java
@@ -0,0 +1,63 @@
+package it.polimi.ingsw.Client.Network;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Message {
+    /**
+     * The type of message [i.e. System;Game;Warning...]
+     */
+    private String type;
+    /**
+     * The method that will be called by the message
+     */
+    private String method;
+    /**
+     * The parameters that will be passed into the method
+     */
+    private ArrayList<Object> params;
+
+    public Message(String type, String method, ArrayList<Object> params) {
+        this.type = type;
+        this.method = method;
+        this.params = params;
+    }
+    public Message(String type, String method) {
+        this.type = type;
+        this.method = method;
+        this.params=new ArrayList<Object>();
+    }
+    public Message(String type, String method, String...params) {
+        this.type=type;
+        this.method=method;
+        this.params=new ArrayList<Object>();
+        this.params.addAll(Arrays.asList(params));
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public ArrayList<Object> getParams() {
+        return params;
+    }
+
+    public void setParams(ArrayList<Object> params) {
+        this.params = params;
+    }
+    public void addParam(Object param) {
+        params.add(param);
+    }
+}
