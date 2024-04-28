5cae2ea8d4b515898448f5118ecbc4197f90500f
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-04-28 15:21:57 +0200

    added comments to DrawableArea

--- src/main/java/it/polimi/ingsw/Server/Model/DrawableArea.java
+++ src/main/java/it/polimi/ingsw/Server/Model/DrawableArea.java
@@ -5,21 +5,40 @@ package it.polimi.ingsw.Server.Model;
  */
 public class DrawableArea {
 
+    /**
+     * contains the gold cards that are visible to all layers and can be drawn,
+     * the first card is always face down and the other two are face up
+     */
     private final DrawableCards goldDrawableCards;
-
+    /**
+     * contains the resource cards that are visible to all layers and can be drawn,
+     * the first card is always face down and the other two are face up
+     */
     private final DrawableCards resourceDrawableCards;
 
-
+    /**
+     *
+     * @param resourceDrawableCards are the resource cards that can be drawn
+     * @param goldDrawableCards are the gold cards that can be drawn
+     */
     public DrawableArea(DrawableCards resourceDrawableCards, DrawableCards goldDrawableCards){
         this.resourceDrawableCards = resourceDrawableCards;
         this.goldDrawableCards = goldDrawableCards;
     }
 
 
+    /**
+     *
+     * @return gold cards that can be drawn
+     */
     public DrawableCards getGoldDrawingSection() {
         return goldDrawableCards;
     }
 
+    /**
+     *
+     * @return resource cards that can be drawn
+     */
     public DrawableCards getResourceDrawingSection() {
         return resourceDrawableCards;
     }
