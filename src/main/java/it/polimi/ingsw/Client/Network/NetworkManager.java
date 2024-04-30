b2305bae18de76bcb45c3ddae4b37ff346ecac9b
Kevin Vallerin <k.vallerin01@gmail.com>
2024-04-30 12:32:15 +0200

    Solved error in previous commit

--- src/main/java/it/polimi/ingsw/Client/Network/NetworkManager.java
+++ src/main/java/it/polimi/ingsw/Client/Network/NetworkManager.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.Client.Network;
 
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class NetworkManager {
     private PrintWriter outSocket;
@@ -9,9 +14,40 @@ public class NetworkManager {
     private BufferedReader inKeyboard;
     private Socket socket;
     private Parser parser;
+    private static final String networkManagerSetupAddress="networkManagerSetup.json";
+
+    /**
+     * setups the network manager getting server and port as input
+     * @param server
+     * @param port
+     * @throws IOException
+     */
     public NetworkManager(String server , int port) throws IOException {
         connect(server, port);
         setupIO();
+    }
+
+    /**
+     * setups the network manager getting server and port from the resources
+     * @throws IOException
+     */
+    public NetworkManager() throws IOException {
+        Map <String,String> settings= getSettings();
+        connect(settings.get("Server"),Integer.parseInt(settings.get("Port")));
+        setupIO();
+    }
+
+    /**
+     * reads networkManagerSetupAddress and extracts the settings
+     * @return the server's settings
+     * @throws IOException
+     */
+    private Map<String,String> getSettings() throws IOException {
+        Gson gson = new Gson();
+        Reader reader = Files.newBufferedReader(Paths.get(networkManagerSetupAddress));
+        Map<String, String> settings = gson.fromJson(reader, Map.class);
+        reader.close();
+        return settings;
     }
 
     /**
