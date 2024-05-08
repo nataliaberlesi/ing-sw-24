2458944671e1570b5b733e52fdf6e29ce3494ff8
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-05-08 14:45:02 +0200

    added placedCards, in sight of persistence

--- src/main/java/it/polimi/ingsw/Server/Model/PlacedCard.java
+++ src/main/java/it/polimi/ingsw/Server/Model/PlacedCard.java
@@ -4,43 +4,18 @@ import it.polimi.ingsw.Server.Model.Cards.Card;
 
 /**
  * every time a card is placed a new place card is made that saves which card was placed, where it was placed and if it's face up
+ *
+ * @param palcedCard      card that was placed
+ * @param cardCoordinates coordinates where card was placed
+ * @param isFacingUp      orientation of card that was placed
  */
-public class PlacedCard {
+public record PlacedCard(Card palcedCard, Coordinates cardCoordinates, boolean isFacingUp) {
 
     /**
-     * card that was placed
-     */
-    private final Card palcedCard;
-    /**
-     * coordinates where card was placed
-     */
-    private final Coordinates cardCoordinates;
-    /**
-     * orientation of card that was placed
-     */
-    private final boolean isFacingUp;
-
-    /**
-     *
-     * @param palcedCard card that was placed
+     * @param palcedCard      card that was placed
      * @param cardCoordinates coordinates of where card was placed
-     * @param isFacingUp orientation  of card that was placed
+     * @param isFacingUp      orientation  of card that was placed
      */
-    public PlacedCard(Card palcedCard, Coordinates cardCoordinates, boolean isFacingUp) {
-        this.palcedCard = palcedCard;
-        this.cardCoordinates = cardCoordinates;
-        this.isFacingUp = isFacingUp;
-    }
-
-    public Card getPalcedCard() {
-        return palcedCard;
-    }
-
-    public Coordinates getCardCoordinates() {
-        return cardCoordinates;
-    }
-
-    public boolean isFacingUp() {
-        return isFacingUp;
+    public PlacedCard {
     }
 }
