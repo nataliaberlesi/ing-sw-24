c4decf6e21332dc17e8153922fcae9ee9eb18403
Kevin Vallerin <k.vallerin01@gmail.com>
2024-05-09 15:28:07 +0200

    GameInstance:added documentation

--- src/main/java/it/polimi/ingsw/Server/Controller/GameInstance.java
+++ src/main/java/it/polimi/ingsw/Server/Controller/GameInstance.java
@@ -5,6 +5,9 @@ import it.polimi.ingsw.Server.Model.Player;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Is the instance of the game for the controller and acts as a way to communicate with the model data
+ */
 public class GameInstance {
     private ArrayList<String> playersTurnOrder;
     private String currentPlayer;
@@ -16,13 +19,29 @@ public class GameInstance {
         players=new HashMap<String,Player>();
         joinPlayer(masterNickname);
     }
+
+    /**
+     * Adds the given username to playersTurnOrder and creates the Player model instance
+     * @param username
+     */
     public void joinPlayer(String username) {
         playersTurnOrder.add(username);
         players.put(username,new Player(username));
     }
+
+    /**
+     * Checks if the given username is already taken
+     * @param username
+     * @return
+     */
     public boolean unavailableUsername(String username) {
         return !(playersTurnOrder.contains(username));
     }
+
+    /**
+     * Checks if the game has reached his full capacity
+     * @return
+     */
     public boolean checkIfGameIsFull() {
         return this.playersTurnOrder.size()== numberOfPlayers;
     }
