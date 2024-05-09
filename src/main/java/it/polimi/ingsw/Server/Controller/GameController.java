0fe2a7b5387d2b2cda3bd5acf7ef77eb84e16554
Kevin Vallerin <k.vallerin01@gmail.com>
2024-05-09 15:02:05 +0200

    GameController: added queryModel method to dispatch messages, added some methods

--- src/main/java/it/polimi/ingsw/Server/Controller/GameController.java
+++ src/main/java/it/polimi/ingsw/Server/Controller/GameController.java
@@ -1,36 +1,109 @@
 package it.polimi.ingsw.Server.Controller;
 
 
-import it.polimi.ingsw.Server.Model.Player;
-import it.polimi.ingsw.Server.Network.Gateway;
-import it.polimi.ingsw.Server.Network.PlayerConnection;
-import it.polimi.ingsw.Server.Network.Server;
+import com.google.gson.JsonObject;
+import it.polimi.ingsw.Server.Controller.DTO.CreateGame;
+import it.polimi.ingsw.Server.Network.*;
 
 import java.io.IOException;
 
 public class GameController {
-
+    private MessageParser messageParser;
+    private GameInstance gameInstance;
     private Server server;
-    private int playerNumber;
-    public void createGame() throws IOException {
-        PlayerConnection MasterConnection= server.getConnections().getFirst();
-        MasterConnection.getGateway().masterStatus(MasterConnection.isMaster());
-        MasterConnection.getGateway().createGame();
-        this.playerNumber=(Integer)MasterConnection.getGateway().getLastParams().get(0);
-        String nickname=(String)MasterConnection.getGateway().getLastParams().get(1);
-        MasterConnection.setPlayer(new Player(nickname));
+    private Parser parser;
+    public GameController() {
+        this.messageParser=new MessageParser();
     }
-    public void startGame() throws IOException {
-        for(int i=1; i<playerNumber;i++) {
-            server.waitPlayer();
-            PlayerConnection currenConnection=server.getConnections().getLast();
-            currenConnection.getGateway().masterStatus(currenConnection.isMaster());
-            String nickname=currenConnection.getGateway().startGame();
-            currenConnection.setPlayer(new Player(nickname));
+
+    public Message dispatchMessage(String message) {
+        MessageType messageType=messageParser.getMessageType(message);
+        String username=messageParser.getUsername(message);
+        String messageParams=messageParser.getMessageParams(message);
+        return queryModel(messageType, username, messageParams);
+    }
+    /**
+     * Given a specific type of message, queries the model to get/change some information
+     */
+    public Message queryModel(MessageType messageType,String username, String messageParams) {
+        JsonObject jsonParams=messageParser.getParser().toJsonObject(messageParams);
+        switch (messageType) {
+            case CREATE -> {
+                return createGame(messageParams);
+            }
+            case JOIN -> {
+                return joinGame(username,messageParams);
+
+            }
+            case START -> {
+
+            }
+            case ACTION -> {
+            }
+            case FINAL_ROUND -> {
+
+            }
+            case ABORT -> {
+
+            }
+            case ERROR -> {
+
+            }
         }
     }
-    public void setServer(Server server) {
-        this.server = server;
+
+    /**
+     * Creates a game
+     */
+    public Message createGame(String messageParams){
+        if(this.gameInstance!=null){
+            MessageType type=MessageType.ERROR;
+            String params="Game is already created";
+            return new Message(type, params);
+        }
+        CreateGame createGame=messageParser.parseCreateGame(messageParams);
+        this.server.setMaxAllowablePlayers(createGame.numberOfPlayers());
+        this.gameInstance=new GameInstance(createGame.username(),createGame.numberOfPlayers());
+        MessageType type=MessageType.CREATE;
+        String params="OK";
+        return new Message(type, params);
+    }
+    public Message joinGame(String username, String messageParams) {
+        Boolean unavailableUsername=this.gameInstance.unavailableUsername(username);
+        if(unavailableUsername) {
+            JsonObject jsonObject=new JsonObject();
+            jsonObject.addProperty("unavailableUsername",unavailableUsername);
+            return this.craftJSONMessage(MessageType.JOIN, jsonObject.toString());
+        }
+        JsonObject jsonObject=new JsonObject();
+        jsonObject.addProperty("unavailableUsername",unavailableUsername);
+        jsonObject.addProperty("full",gameInstance.checkIfGameIsFull());
+        jsonObject.addProperty("username",username);
+        return this.craftJSONMessage(MessageType.JOIN, jsonObject.toString());
+
+    }
+    public boolean gameIsFull() {
+        return this.gameInstance.checkIfGameIsFull();
+    }
+    public boolean gameIsStarted() {
+        return this.gameIsStarted();
+    }
+    public Message craftJSONMessage(MessageType messageType, String params) {
+        return new Message(messageType, params);
+    }
+
+    /**
+     * Starts a game when all user are joined
+     * @throws IOException
+     */
+    public void startGame() throws IOException {
+    }
+
+    /**
+     *
+     * @throws IOException
+     */
+    public void chooseColor() throws IOException {
     }
     public void shutDown() {
 
