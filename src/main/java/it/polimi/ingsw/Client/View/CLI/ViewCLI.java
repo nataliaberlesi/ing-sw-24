96f15a2dfe3f9cbcc62e9b0a576c1f4275c5f0a1
natalia <natalia.berlesidevasoncellos@mail.polimi.it>
2024-05-10 15:18:46 +0200

    Extended View in ViewCLi

--- src/main/java/it/polimi/ingsw/Client/View/CLI/ViewCLI.java
+++ src/main/java/it/polimi/ingsw/Client/View/CLI/ViewCLI.java
@@ -1,8 +1,95 @@
 package it.polimi.ingsw.Client.View.CLI;
 
+import it.polimi.ingsw.Client.Network.MessageDispatcher;
+import it.polimi.ingsw.Client.Network.MessageParser;
 import it.polimi.ingsw.Client.View.View;
+
+import java.util.List;
 
 public class ViewCLI extends View {
 
 
+    protected ViewCLI(MessageParser messageParser, MessageDispatcher messageDispatcher) {
+        super(messageParser, messageDispatcher);
+    }
+
+    @Override
+    protected void waitForStart() {
+
+    }
+
+    @Override
+    protected void createGame() {
+
+    }
+
+    @Override
+    protected void joinGame() {
+
+    }
+
+    @Override
+    protected void startGame() {
+
+    }
+
+    @Override
+    protected void startShow() {
+
+    }
+
+    @Override
+    protected void switchToCreate() {
+
+    }
+
+    @Override
+    protected void switchToJoin() {
+
+    }
+
+    @Override
+    protected void updateGame() {
+
+    }
+
+    @Override
+    protected void showErrorAlert(String header, String content) {
+
+    }
+
+    @Override
+    protected void showAbort(String message) {
+
+    }
+
+    @Override
+    protected void showError(String message) {
+
+    }
+
+    @Override
+    protected void closeGame(List<String> winners) {
+
+    }
+
+    @Override
+    protected void enableActions() {
+
+    }
+
+    @Override
+    protected void waitTurn() {
+
+    }
+
+    @Override
+    protected void returnToMainMenu() {
+
+    }
+
+    @Override
+    public void main(String[] args) {
+
+    }
 }
