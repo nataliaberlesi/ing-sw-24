2aefcc52342dd7787ee73d31e30db9e130794f9a
Kevin Vallerin <k.vallerin01@gmail.com>
2024-05-16 21:25:44 +0200

    Added hand parsing

--- src/main/java/it/polimi/ingsw/Client/Network/MessageParser.java
+++ src/main/java/it/polimi/ingsw/Client/Network/MessageParser.java
@@ -19,20 +19,42 @@ import java.util.NoSuchElementException;
  * Used to parse data from the incoming message from server
  * */
 public class MessageParser {
+    /**
+     * Instance of the networkManager gateway to the server
+     */
     private NetworkManager networkManager;
+    /**
+     * Singleton instance of this
+     */
     private static MessageParser instance;
+    /**
+     * viewController handled by this messageParser
+     */
     private ViewController viewController;
+    /**
+     * Gson instance for message parsing
+     */
     private Gson parser;
-    private JsonObject currentMessage;
-    private JsonObject messageParams;
+    /**
+     * Current input parameters handled by the messageParser
+     */
     private InParamsDTO inParamsDTO;
+    /**
+     * Current messageType handled by the messageParser
+     */
     private MessageType messageType;
 
 
-    public MessageParser(NetworkManager networkManager) {
+    private MessageParser(NetworkManager networkManager) {
         this.networkManager=networkManager;
         this.parser=new Gson();
     }
+
+    /**
+     * Used to get the singleton instance
+     * @param networkManager
+     * @return
+     */
     public static MessageParser getInstance(NetworkManager networkManager) {
         if(instance==null) {
             instance= new MessageParser(networkManager);
@@ -42,6 +64,11 @@ public class MessageParser {
     public MessageType getMessageType() {
         return messageType;
     }
+
+    /**
+     * Sets the current message data. From now on until the message is consumed the message will remain the same
+     * @param inMessage
+     */
     public void buildMessage(String inMessage) {
         Message message=parser.fromJson(inMessage,Message.class);
         this.messageType=message.type();
@@ -104,21 +131,6 @@ public class MessageParser {
     public ArrayList<String> getPlayers() {
         return inParamsDTO.players();
     }
-    public String getCardID() {
-        return inParamsDTO.card().cardID();
-    }
-    public String getCardID(String drawableArea, int index) {
-        switch (drawableArea) {
-            case("resourceDrawableArea")->{
-                return inParamsDTO.resourceDrawableArea()[index].cardID();
-            }
-            case("goldDrawableArea")->{
-                return inParamsDTO.goldDrawableArea()[index].cardID();
-            }
-        }
-        throw new MessageParserException("Not such case exception: "+drawableArea);
-    }
-
     /**
      * Parses the list of available colors to choose
      * @return
@@ -141,6 +153,13 @@ public class MessageParser {
     private CardDTO getCardDTO() {
         return inParamsDTO.card();
     }
+
+    /**
+     * Gets a cardDTO from a given drawableArea
+     * @param drawableArea the given drawableArea
+     * @param index the index inside the drawableArea
+     * @return
+     */
     private CardDTO getCardDTO(String drawableArea, int index) {
         switch(drawableArea) {
             case("goldDrawableArea") -> {
@@ -152,6 +171,10 @@ public class MessageParser {
         } throw new MessageParserException("Not such drawableArea:"+ drawableArea);
     }
 
+    /**
+     * Parses a CardCLI from the message
+     * @return
+     */
     public CardCLI getCardCLI() {
         CardDTO cardDTO=getCardDTO();
         ArrayList<String> prerequisites=new ArrayList<>();
@@ -168,6 +191,11 @@ public class MessageParser {
                 prerequisites
         );
     }
+
+    /**
+     * Parses the placed cards in CLI format
+     * @return an ordered ArrayList of PlacedCards in CLI format
+     */
     public ArrayList<CardCLI> getPlacedCardsCLI() {
         ArrayList<PlacedCardDTO> placedCardsDTO=inParamsDTO.placedCards();
         ArrayList<CardCLI> placedCardsCLI=new ArrayList<>();
@@ -191,7 +219,12 @@ public class MessageParser {
         }
         return placedCardsCLI;
     }
-    /*public ArrayList<CardGUI> getPlacedCardsGUI() {
+
+    /**
+     * Parses the placed cards in GUI format
+     * @return and ordered ArrayList of gui cards
+     */
+    public ArrayList<CardGUI> getPlacedCardsGUI() {
         ArrayList<CardGUI> placedCardsGUI=new ArrayList<>();
         ArrayList<PlacedCardDTO> placedCardsDTO=inParamsDTO.placedCards();
         for(PlacedCardDTO placedCardDTO: placedCardsDTO) {
@@ -205,7 +238,14 @@ public class MessageParser {
             );
         }
         return placedCardsGUI;
-    }*/
+    }
+
+    /**
+     * Parses a Card in CLI format from a given drawableArea
+     * @param drawingSection the given drawableArea
+     * @param index the index in the drawableArea
+     * @return a Card in CLI format
+     */
     public CardCLI getCardCLI(String drawingSection, int index) {
         CardDTO cardDTO=getCardDTO(drawingSection,index);
         ArrayList<String> prerequisites=new ArrayList<>();
@@ -222,6 +262,11 @@ public class MessageParser {
                 prerequisites
         );
     }
+
+    /**
+     * Parses the starting card in CLI format
+     * @return a starting card in CLI format
+     */
     public CardCLI getStartingCardCLI() {
         CardDTO cardDTO=getCardDTO();
         return new CardCLI(
@@ -231,9 +276,72 @@ public class MessageParser {
                 cardDTO.frontCenterSymbols()
         );
     }
+    public CardCLI[] getHandCLI() {
+        CardCLI[] handCLI=new CardCLI[3];
+        CardDTO[] handDTO=inParamsDTO.hand();
+        for(int i=0;i<3;i++) {
+            handCLI[i]=
+                    new CardCLI(
+                            handDTO[i].cardID(),
+                            handDTO[i].frontCorners(),
+                            handDTO[i].backCorners(),
+                            handDTO[i].cardObjective().type(),
+                            handDTO[i].cardObjective().data().symbolOfInterest(),
+                            handDTO[i].cardObjective().data().points(),
+                            handDTO[i].prerequisites()
+                    );
+        }
+        return handCLI;
+    }
+    public String[] getHandIDs() {
+        String[] handIDs=new String[3];
+        CardDTO[] handDTO=inParamsDTO.hand();
+        for(int i=0;i<3;i++) {
+            handIDs[i]=
+                    handDTO[i].cardID();
+        }
+        return handIDs;
+    }
+    /**
+     * Parses the starting card ID
+     * @return
+     */
+    public String getCardID() {
+        return inParamsDTO.card().cardID();
+    }
+
+    /**
+     * Parses the card ID from a given drawableArea
+     * @param drawableArea the given drawableArea
+     * @param index the index inside the drawableArea
+     * @return
+     */
+    public String getCardID(String drawableArea, int index) {
+        switch (drawableArea) {
+            case("resourceDrawableArea")->{
+                return inParamsDTO.resourceDrawableArea()[index].cardID();
+            }
+            case("goldDrawableArea")->{
+                return inParamsDTO.goldDrawableArea()[index].cardID();
+            }
+        }
+        throw new MessageParserException("Not such case exception: "+drawableArea);
+    }
+
+    /**
+     * Transforms an Object into a navigable JsonObject
+     * @param object
+     * @return
+     */
     public JsonObject toJsonObject(Object object) {
         return parser.toJsonTree(object).getAsJsonObject();
     }
+
+    /**
+     * Transforms an Object into a json string
+     * @param object
+     * @return
+     */
     public String toJson(Object object) {
         return parser.toJson(object);
     }
