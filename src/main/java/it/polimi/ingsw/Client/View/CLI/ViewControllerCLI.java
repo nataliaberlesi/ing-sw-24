9b616dfe39f91cf6bb2497855cb27d2cfd0adb00
zerowat02 <giuliano.croppi@mail.polimi.it>
2024-05-20 23:37:42 +0200

    implemented cli termination

--- src/main/java/it/polimi/ingsw/Client/View/CLI/ViewControllerCLI.java
+++ src/main/java/it/polimi/ingsw/Client/View/CLI/ViewControllerCLI.java
@@ -88,6 +88,7 @@ public class ViewControllerCLI extends ViewController {
         clientInputHandler.terminate();
         System.out.print("\033[H\033[2J");
         System.out.flush();
+        System.exit(0);
     }
 
     @Override
