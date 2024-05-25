b6efe04b3eb19bedf69c769e71a9db73d59d6b8e
Kevin Vallerin <k.vallerin01@gmail.com>
2024-05-25 13:51:53 +0200

    bug-fix: gameinstance were not open to gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -14,6 +14,7 @@ module it.polimi.ingsw {
     opens it.polimi.ingsw.Server.Model.Cards.Objectives to com.google.gson;
     opens it.polimi.ingsw.Server.Model to com.google.gson;
     opens it.polimi.ingsw.Client.Network.DTO to com.google.gson;
+    opens it.polimi.ingsw.Server.Controller to com.google.gson;
     // I need to add the path of the directory containing the GUIApplication class to the module for it to work
     opens it.polimi.ingsw.Client.View.GUI to javafx.fxml;
     exports it.polimi.ingsw.Client.View.GUI;
